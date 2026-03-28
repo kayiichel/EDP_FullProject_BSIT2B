7efd299a8fb7f0ec5881e35651bbe70a6959f334
kayiichel <kayiichel@gmail.com>
2026-03-28 23:10:55 +0800

    update

--- WindowsFormsApp3/Form2.Designer.cs
+++ WindowsFormsApp3/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp3
 {
-    partial class frmHome
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -46,23 +46,24 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.Controls.Add(this.button6, 2, 1);
             this.tableLayoutPanel1.Controls.Add(this.button5, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(126, 88);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(44, 92);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(553, 261);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(694, 257);
             this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 124);
+            this.button1.Size = new System.Drawing.Size(225, 122);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -70,62 +71,57 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(187, 3);
+            this.button2.Location = new System.Drawing.Point(234, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(178, 124);
+            this.button2.Size = new System.Drawing.Size(225, 122);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(371, 3);
+            this.button3.Location = new System.Drawing.Point(465, 3);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(178, 124);
+            this.button3.Size = new System.Drawing.Size(226, 122);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(3, 133);
+            this.button4.Location = new System.Drawing.Point(3, 131);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(178, 124);
+            this.button4.Size = new System.Drawing.Size(225, 123);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(187, 133);
+            this.button5.Location = new System.Drawing.Point(234, 131);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(178, 124);
-            this.button5.TabIndex = 4;
+            this.button5.Size = new System.Drawing.Size(225, 123);
+            this.button5.TabIndex = 5;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(371, 133);
+            this.button6.Location = new System.Drawing.Point(465, 131);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(178, 124);
-            this.button6.TabIndex = 5;
+            this.button6.Size = new System.Drawing.Size(226, 123);
+            this.button6.TabIndex = 7;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // frmHome
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Name = "frmHome";
-            this.Text = "frmHome";
+            this.Name = "Form2";
+            this.Text = "Form2";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -134,11 +130,11 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
     }
 }
--- WindowsFormsApp3/frmHome.cs
+++ WindowsFormsApp3/frmHome.cs
@@ -16,5 +16,41 @@ namespace WindowsFormsApp3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form3().Show();
+            this.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new Form4().Show();
+            this.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Form5().Show();
+            this.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            new Form6().Show();
+            this.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            new Form7().Show();
+            this.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            new Form8().Show();
+            this.Show();
+        }
     }
 }
